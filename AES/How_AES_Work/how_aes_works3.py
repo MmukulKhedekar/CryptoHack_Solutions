85eebf3411a19e201f1713c7f01d09212dd6b8e2
MmukulKhedekar <36923816+MmukulKhedekar@users.noreply.github.com>
2023-05-02 20:24:21 +0530

    Add files via upload

---
+++ AES/How_AES_Work/how_aes_works3.py
@@ -0,0 +1,25 @@
+def bytes2matrix(text):
+    """ Converts a 16-byte array into a 4x4 matrix.  """
+    return [list(text[i:i+4]) for i in range(0, len(text), 4)]
+
+def matrix2bytes(matrix):
+    """ Converts a 4x4 matrix into a 16-byte array.  """
+    array=[]
+    for i in range(4):
+        for j in range(4):
+            array.append(matrix[i][j])
+    return array
+
+matrix = [
+    [99, 114, 121, 112],
+    [116, 111, 123, 105],
+    [110, 109, 97, 116],
+    [114, 105, 120, 125],
+]
+
+print(matrix2bytes(matrix))
+new_matrix=matrix2bytes(matrix)
+for i in new_matrix:
+    print(chr(i),end="")
+    
+# crypto{inmatrix}
