b020ddd663ba0d2b050851beed0749dd2aa09749
MmukulKhedekar <36923816+MmukulKhedekar@users.noreply.github.com>
2023-05-02 19:34:18 +0530

    Update problem3.py

--- Introduction/problem3.py
+++ Introduction/problem3.py
@@ -1,3 +1,11 @@
-# send {"buy":"flag"}
+from pwn import *
+host, port='socket.cryptohack.org', 11112
+r = remote(host,port)
 
-# crypto{sh0pp1ng_f0r_fl4g5}
+res = (r.recv())
+r.sendline(b'{"buy":"flag"}')
+rem = (r.recvline())
+print(rem)
+r.close()
+
+# b'{"flag": "crypto{sh0pp1ng_f0r_fl4g5}"}\n'
