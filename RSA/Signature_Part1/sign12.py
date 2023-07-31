59664cac642c2e78fda3605a9c53befc6112eb66
MmukulKhedekar <36923816+MmukulKhedekar@users.noreply.github.com>
2023-07-31 18:20:04 +0530

    okay

---
+++ RSA/Signature_Part1/sign12.py
@@ -0,0 +1,12 @@
+from pwn import *
+from Cryptodome.Util.number import *
+r = remote('socket.cryptohack.org', 13374)
+print(r.recvline())
+r.sendline(b'{"option": "get_secret"}')
+x = eval(r.recvline().decode().strip('\n'))["secret"]
+print(x)
+r.sendline(b'{"option": "sign", "msg": \"' + str(x).encode() + b'\"}')
+x = int(eval(r.recvline().decode().strip('\n'))["signature"][2:],16)
+print(long_to_bytes(x))
+
+# crypto{d0n7_516n_ju57_4ny7h1n6}
